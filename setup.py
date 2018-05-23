86d827955af952ae99a390ecaee22389ff2af0ea
marcusfuchs <mfuchs@eonerc.rwth-aachen.de>
2018-05-23 21:18:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 import setuptools
 
 setuptools.setup(name='uesgraphs',
-                 version='0.5.2',
+                 version='0.6.0',
                  description='Graphs to describe Urban Energy Systems',
                  url='https://github.com/RWTH-EBC/uesgraphs',
                  author='Marcus Fuchs',
                  author_email='mfuchs@eonerc.rwth-aachen.de',
-                 license='Will be added soon',
+                 license='MIT License',
                  packages=['uesgraphs'],
                  install_requires=['pytest', 'networkx', 'numpy', 'pandas',
                                    'shapely', 'pyproj', 'matplotlib',
