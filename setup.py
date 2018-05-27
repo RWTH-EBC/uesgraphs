ec4db10f739a482cdf7f7cbcae2c7f7460b0cd59
Marcus Fuchs <mfuchs@eonerc.rwth-aachen.de>
2018-05-27 17:52:44 +0200

    Fix missing packages in setup
    For #1

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 import setuptools
 
 setuptools.setup(name='uesgraphs',
-                 version='0.6.0',
+                 version='0.6.1',
                  description='Graphs to describe Urban Energy Systems',
                  url='https://github.com/RWTH-EBC/uesgraphs',
                  author='Marcus Fuchs',
                  author_email='mfuchs@eonerc.rwth-aachen.de',
                  license='MIT License',
-                 packages=['uesgraphs'],
+                 packages=setuptools.find_packages(),
                  install_requires=['pytest', 'networkx', 'numpy', 'pandas',
                                    'shapely', 'pyproj', 'matplotlib',
-                                   'nose', 'pytest-mpl',])
+                                   'nose', 'pytest-mpl', ],
+                 classifiers=("Programming Language :: Python :: 3", ),
+                 )
+
